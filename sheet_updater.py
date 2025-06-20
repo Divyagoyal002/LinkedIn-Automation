c22d9ec0cc6c7398163bc2fd38ed735905b42572
Divya Goyal <92865540+Divyagoyal002@users.noreply.github.com>
2025-06-20 14:56:33 +0530

    updated_sheet_updater

--- sheet_updater.py
+++ sheet_updater.py
@@ -2,9 +2,10 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 def push_to_sheet(content):
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("phantombuster_config.json", scope)
+  
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('/home/multi-sy-9/Desktop/Onetab/Ln_Automation/phantombuster_config.json', scope)
     client = gspread.authorize(creds)
-    
-    sheet = client.open("LinkedIn Auto Posts").sheet1
+    sheet = client.open("LinkedIn Auto Posts").sheet1 
     sheet.append_row([content])
+    sheet.delete_rows(2)  # Use 1 if there are no headers
